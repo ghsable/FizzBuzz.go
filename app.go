1b178596e74f608408a3396a069be02d37c065fb
dativus <sn.sable005@gmail.com>
2022-08-23 23:58:20 +0900

    add app.go

---
+++ app.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+    "strconv"
+)
+
+func getPrint(n int) string {
+    if (n%15 == 0) {
+        return "Fizz Buzz"
+    } else if (n%5 == 0) {
+        return "Buzz"
+    } else if (n%3 == 0) {
+        return "Fizz"
+    } else {
+        return strconv.Itoa(n)
+    }
+}
